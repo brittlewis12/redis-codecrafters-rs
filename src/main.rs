a83a4e03354e6c4779042f2cbb265c9cd332163d
Britt Lewis <brittlewis12@gmail.com>
2024-03-26 22:13:45 -0400

    Use tokio for async net, io interfaces

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
-use std::{
-    io::{Read, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, Result},
     net::TcpListener,
 };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        println!("accepted new connection");
-        let mut buf = [0; 512];
-        loop {
-            if let Ok(len) = stream.read(&mut buf) {
-                if len == 0 {
-                    break;
-                }
-                println!("received: {:?}", String::from_utf8_lossy(&buf[..len]));
-                let response = match &buf[..len] {
-                    b"*1\r\n$4\r\nping\r\n" => "+PONG\r\n",
-                    _ => "-ERR unknown command\r\n",
-                };
-                stream.write_all(response.as_bytes()).unwrap();
-                stream.flush().unwrap();
+    let (mut stream, _addr) = listener.accept().await.unwrap();
+    println!("accepted new connection");
+    let mut buf = [0; 512];
+    loop {
+        if let Ok(len) = stream.read(&mut buf).await {
+            if len == 0 {
+                break;
             }
+            println!("received: {:?}", String::from_utf8_lossy(&buf[..len]));
+            let response = match &buf[..len] {
+                b"*1\r\n$4\r\nping\r\n" => "+PONG\r\n",
+                _ => "-ERR unknown command\r\n",
+            };
+            stream.write_all(response.as_bytes()).await.unwrap();
+            stream.flush().await.unwrap();
         }
     }
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::io::{Read, Write};
     use std::net::TcpStream;
 
     #[test]
