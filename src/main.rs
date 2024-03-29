a38525f8feabb60a53f377258e9124001cc0e15c
Britt Lewis <brittlewis12@gmail.com>
2024-03-28 21:28:51 -0400

    Preliminary RESP bulk string & array parsing

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,93 @@ async fn main() -> Result<()> {
         let (mut stream, _addr) = listener.accept().await.unwrap();
         println!("accepted new connection");
         tokio::spawn(async move {
-            let mut buf = [0; 512];
             loop {
-                if let Ok(len) = stream.read(&mut buf).await {
-                    if len == 0 {
-                        break;
-                    }
-                    println!("received: {:?}", String::from_utf8_lossy(&buf[..len]));
-                    let response = match &buf[..len] {
-                        b"*1\r\n$4\r\nping\r\n" => "+PONG\r\n",
-                        _ => "-ERR unknown command\r\n",
-                    };
-                    stream.write_all(response.as_bytes()).await.unwrap();
-                    stream.flush().await.unwrap();
+                let mut buf = vec![0; 512];
+                let len = stream.read(&mut buf).await.unwrap();
+                if len == 0 {
+                    break;
                 }
+                let input = std::str::from_utf8(&buf[..len]).expect("invalid utf8 string");
+                println!("received: {:?}", input);
+                let command = parse(&input).unwrap();
+                let response = match &buf[..len] {
+                    b"*1\r\n$4\r\nping\r\n" => "+PONG\r\n",
+                    _ => "-ERR unknown command\r\n",
+                };
+                stream.write_all(response.as_bytes()).await.unwrap();
+                stream.flush().await.unwrap();
             }
         });
     }
 }
 
+
+// fn parse_line(line: String) -> Result<DataType> { }
+
+pub(crate) fn parse(input: &str) -> Result<(DataType, &str)> {
+    // check first byte. we'll handle +, $, * for now.
+    let (first_byte, rest) = input.split_at(1);
+    let first_char = first_byte.chars().next().expect("unexpected empty string");
+    println!("first char: {}", &first_char);
+    let data = match first_char {
+        '+' => {
+            // parse simple string
+            let (simple_string, rest) = rest.split_once("\r\n").expect("missing \\r\\n terminator for simple string");
+            (DataType::SimpleString(simple_string.to_string()), rest)
+        }
+        '$' => {
+            let (str_len, rest) = rest.split_once("\r\n").expect("missing \\r\\n terminator for bulk string");
+            let len: usize = str_len.parse().expect("invalid bulk string length");
+            println!("bulk string detected. len: {len}");
+            let bulk_string = &rest[..len];
+            println!("bulk string: {:?}", bulk_string);
+            // skip \r\n & panic if not found
+            let (crlf, rest) = rest.split_at(len + 2);
+            assert_eq!(crlf, format!("{bulk_string}\r\n"));
+            (DataType::BulkString(bulk_string.to_string()), rest)
+        }
+        '*' => {
+            let (str_len, mut rest) = rest.split_once("\r\n").expect("missing \\r\\n terminator for array");
+            let len: usize = str_len.parse().expect("invalid array length");
+            println!("array detected. len: {len}");
+            // recursively parse array elements by \r\n
+            let mut arr = Vec::with_capacity(len);
+            for _ in 0..len {
+                let (element, newRest) = parse(&rest)?;
+                println!("element: {element:?}     rest: {rest}");
+                rest = newRest;
+                arr.push(element);
+            }
+            println!("array: {arr:?}   rest: {rest}");
+
+            (DataType::Array(arr), rest)
+        }
+        _ => {
+            eprintln!("unimplemented type: {:?}", first_char);
+            unimplemented!()
+        }
+    };
+
+    Ok(data)
+}
+
+/// Redis commands
+pub(crate) enum Command {
+    Ping,
+    Echo,
+}
+
+/// RESP (Redis serialization protocol) types
+#[derive(Debug, PartialEq)]
+pub(crate) enum DataType {
+    SimpleString(String),
+    BulkString(String),
+    Array(Vec<DataType>),
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::io::{Read, Write};
     use std::net::TcpStream;
 
@@ -63,5 +129,43 @@ mod tests {
         let len = stream.read(&mut buf).unwrap();
         assert_eq!(len, 7);
         assert_eq!(String::from_utf8_lossy(&buf[..len]), "+PONG\r\n");
+    }
+
+    #[test]
+    fn test_echo() {
+        let mut stream = TcpStream::connect("127.0.0.1:6379").unwrap();
+        stream.write_all(b"*2\r\n$4\r\necho\r\n$3\r\nhey\r\n").unwrap();
+        stream.flush().unwrap();
+        let mut buf = vec![0; 512];
+        let len = stream.read_to_end(&mut buf).unwrap();
+        assert_eq!("hey", String::from_utf8_lossy(&mut buf[..len]));
+
+    }
+
+    #[test]
+    fn test_parse_simple_string() {
+        let data = "+OK\r\n";
+        let (parsed, _rest) = parse(data).unwrap();
+        assert_eq!(parsed, DataType::SimpleString("OK".to_string()));
+    }
+
+    #[test]
+    fn test_parse_bulk_string() {
+        let data = "$5\r\nhello\r\n";
+        let (parsed, _rest) = parse(data).unwrap();
+        assert_eq!(parsed, DataType::BulkString("hello".to_string()));
+    }
+
+    #[test]
+    fn test_parse_array() {
+        let data = "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n";
+        let (parsed, _rest) = parse(data).unwrap();
+        assert_eq!(
+            parsed,
+            DataType::Array(vec![
+                DataType::BulkString("foo".to_string()),
+                DataType::BulkString("bar".to_string())
+            ])
+        );
     }
 }
